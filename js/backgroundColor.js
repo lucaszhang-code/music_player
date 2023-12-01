5277d388d971e31d81f3c999e5807fa948c9b430
所念皆星河 <1357513467@qq.com>
2023-12-01 14:08:56 +0800

    优化歌词颜色显示

--- js/backgroundColor.js
+++ js/backgroundColor.js
@@ -1,13 +1,19 @@
 function initializeBackgroundColorUpdater() {
   const img = document.querySelector(".cover img");
+
   function updateColorsAndStyles() {
-    setColorsFromImage(img);
-    updateCachedColors();
+    if (img.complete && img.naturalWidth !== 0) {
+      const dominantColor = getDominantColorFromImage(img);
+      const textColor = getTextColorBasedOnBgColor(dominantColor);
+
+      if (dominantColor && textColor) {
+        applyColorsToPage(dominantColor, textColor);
+      }
+    }
   }
-  if (img.complete && img.naturalWidth !== 0) {
-    updateColorsAndStyles();
-  }
+
   img.onload = updateColorsAndStyles;
+
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.attributeName === "src") {
@@ -15,50 +21,31 @@ function initializeBackgroundColorUpdater() {
       }
     });
   });
+
   observer.observe(img, { attributes: true, attributeFilter: ["src"] });
 }
 
-function setColorsFromImage(img) {
-  const dominantColor = getDominantColorFromImage(img);
-  const textColor = getTextColorBasedOnBgColor(dominantColor);
+function applyColorsToPage(dominantColor, textColor) {
+  const rgbDominantColor = rgbToString(dominantColor);
+  const rgbTextColor = rgbToString(textColor);
 
-  if (dominantColor && textColor) {
-    const rgbDominantColor = rgbToString(dominantColor);
-    const rgbTextColor = rgbToString(textColor);
-    const listItems = document.querySelectorAll(".list ul li");
-    const icons = document.querySelectorAll(".icon-geci, .icon-liebiao");
+  document.body.style.backgroundColor = rgbDominantColor;
+  document.body.style.color = rgbTextColor;
 
-    document.body.style.backgroundColor = rgbDominantColor;
-    document.body.style.color = rgbTextColor;
-    document.querySelector(".lrc-container").style.backgroundColor =
-      rgbDominantColor;
-    document.querySelector(".list").style.backgroundColor = rgbDominantColor;
-    // document.querySelector(" .container-info").style.backgroundColor =
-    //   rgbDominantColor;
+  // Update other elements as needed
+  const listItems = document.querySelectorAll(".list ul li");
+  const icons = document.querySelectorAll(".icon-geci, .icon-liebiao");
 
-    for (let li of listItems) {
-      li.style.borderColor = rgbTextColor;
-    }
-
-    for (let icon of icons) {
-      icon.style.color = rgbTextColor;
-    }
-  }
+  listItems.forEach(li => li.style.borderColor = rgbTextColor);
+  icons.forEach(icon => icon.style.color = rgbTextColor);
 }
 
 function rgbToString(rgbArray) {
-  if (!Array.isArray(rgbArray) || rgbArray.length !== 3) {
-    console.error("Invalid RGB array:", rgbArray);
-    return "rgb(0,0,0)";
-  }
-  const [r, g, b] = rgbArray;
-  return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${rgbArray.join(", ")})`;
 }
 
 function getTextColorBasedOnBgColor(bgColor) {
-  if (!bgColor) return null;
-  const luminance =
-    0.299 * bgColor[0] + 0.587 * bgColor[1] + 0.114 * bgColor[2];
+  const luminance = 0.299 * bgColor[0] + 0.587 * bgColor[1] + 0.114 * bgColor[2];
   return luminance > 186 ? [0, 0, 0] : [255, 255, 255];
 }
 
@@ -67,32 +54,25 @@ function getDominantColorFromImage(img) {
     const colorThief = new ColorThief();
     return colorThief.getColor(img);
   } catch (e) {
-    // console.error("Failed to get dominant color:", e);
+    console.error("Failed to get dominant color:", e);
     return null;
   }
 }
 
-let cachedDominantColor = null;
-let cachedTextColor = null;
-
-function updateCachedColors() {
-  const img = document.querySelector(".cover img");
-  cachedDominantColor = getDominantColorFromImage(img);
-  cachedTextColor = getTextColorBasedOnBgColor(cachedDominantColor);
-}
+const defaultLyricColor = "#666666";
 
 function updateActiveLyricColor(index, dom) {
-  let li = dom.ul.querySelector(".lrc-container .lrc-active");
+  let li = dom.ul.querySelector(".lrc-active");
   if (li) {
     li.classList.remove("lrc-active");
     li.style.color = defaultLyricColor;
   }
+
   li = dom.ul.children[index];
-  if (li && cachedTextColor) {
+  if (li) {
     li.classList.add("lrc-active");
-    li.style.color = rgbToString(cachedTextColor);
+    li.style.color = document.body.style.color;
   }
 }
 
-const defaultLyricColor = "#666666";
 window.addEventListener("load", initializeBackgroundColorUpdater);
